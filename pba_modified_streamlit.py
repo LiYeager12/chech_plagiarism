45d81dbce160ecae7eadb1bf8ff3895090cd20a1
LiYeager12 <lailiafi12@gmail.com>
2024-06-20 21:14:51 +0700

    Update pba_modified_streamlit.py

--- pba_modified_streamlit.py
+++ pba_modified_streamlit.py
@@ -1,14 +1,14 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import SVC
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.model_selection import train_test_split
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.svm import SVC
+# from sklearn.metrics.pairwise import cosine_similarity
+# from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import re
 import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
+# from nltk.corpus import stopwords
+# from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 import streamlit as st
 
